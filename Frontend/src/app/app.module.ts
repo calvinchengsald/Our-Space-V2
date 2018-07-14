b3a862372d789174f1b3212261a42dcdf758313e
Tylor Clemons <tylorclem@gmail.com>
2018-07-14 16:25:18 -0400

    Login Created
    Created Login component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatFormFieldModule, MatSelectModule, MatCardModule, MatButtonModule } from '@angular/material';
+
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,
+    MatInputModule, MatFormFieldModule, MatSelectModule,
+    MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
