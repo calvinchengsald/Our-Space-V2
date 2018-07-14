953fad4a79d209926af3a1bfd96b2c4855d7c877
Tylor Clemons <tylorclem@gmail.com>
2018-07-14 17:04:26 -0400

    Merged frontend in tylor_frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatFormFieldModule, MatSelectModule, MatCardModule, MatButtonModule } from '@angular/material';
-
-
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-=======
 import { HttpModule } from '@angular/http';
-import { FormsModule }   from '@angular/forms';
-import {RouterModule, Router} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule, MatSelectModule, MatCardModule, MatButtonModule, MatFormFieldModule} from '@angular/material';
+
 
 import { LoginService } from './services/login.service';
 
@@ -20,19 +15,10 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterComponent } from './register/register.component';
->>>>>>> frontend
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule, BrowserAnimationsModule,
-    MatInputModule, MatFormFieldModule, MatSelectModule,
-    MatCardModule, MatButtonModule
-=======
     HomeComponent,
     NavbarComponent,
     LoginPageComponent,
@@ -42,16 +28,20 @@ import { RegisterComponent } from './register/register.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
+    MatInputModule, MatSelectModule,
+    MatCardModule, MatButtonModule,
+    MatFormFieldModule,
+
     RouterModule.forRoot([
-      {path: 'login', component:LoginPageComponent},
-      {path: 'register', component:RegisterComponent},
-      {path: '', component:HomeComponent,pathMatch:'full'},
-      {path: '**', component:HomeComponent,pathMatch:'full'},
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '**', component: HomeComponent, pathMatch: 'full' },
     ])
->>>>>>> frontend
   ],
   providers: [LoginService],
   bootstrap: [AppComponent],
-  
+
 })
 export class AppModule { }
