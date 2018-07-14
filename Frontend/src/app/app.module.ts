37d04ab33c8817d6758918ed30fcceb4bd9336e7
Kevin Huang <kevinqkh@gmail.com>
2018-07-14 16:16:31 -0400

    Initial skeleton

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
+import {RouterModule, Router} from '@angular/router';
+
+import { LoginService } from './services/login.service';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'login', component:LoginPageComponent},
+      {path: 'register', component:RegisterComponent},
+      {path: '', component:HomeComponent,pathMatch:'full'},
+      {path: '**', component:HomeComponent,pathMatch:'full'},
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LoginService],
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
