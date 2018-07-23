073ff8617f1c87ed57419f9accd2efa974145b05
Kevin Huang <kevinqkh@gmail.com>
2018-07-23 15:54:52 -0400

    fix

--- Backend/src/main/java/aspect/checkSession.java
+++ Backend/src/main/java/aspect/checkSession.java
@@ -8,9 +8,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseBody;
 
