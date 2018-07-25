4accb5494bfd2dd2e024ec0e5d517868d559d6a6
Jerry Affricot <jerry.affricot@gmail.com>
2018-07-25 19:43:14 -0400

    styling changes

--- Frontend/src/app/services/environment.service.ts
+++ Frontend/src/app/services/environment.service.ts
@@ -7,13 +7,10 @@ export class EnvironmentService {
 
 
 
-<<<<<<< HEAD
-  // static APIpath = 'http://localhost:9005/OurSpace/';
+
+  // static APIpath = 'http://localhost:9001/OurSpace/';
    static APIpath = 'http://ec2-54-145-139-170.compute-1.amazonaws.com:8080/';
-=======
-  static APIpath = 'http://localhost:9001/OurSpace/';
-  //  static APIpath = 'http://ec2-54-145-139-170.compute-1.amazonaws.com:8080/';
->>>>>>> e48b2340a3932a4207b5fb107b3cd67d997f74be
+
 
 
   constructor() { }
