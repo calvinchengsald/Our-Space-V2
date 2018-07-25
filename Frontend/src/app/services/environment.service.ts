177a7766ae008dd6101e2a7788a9bf0edc0a405f
Mac <mackubena@gmail.com>
2018-07-25 13:19:33 -0400

    comitting changes

--- Frontend/src/app/services/environment.service.ts
+++ Frontend/src/app/services/environment.service.ts
@@ -5,20 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class EnvironmentService {
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
   // static APIpath = 'http://localhost:9005/OurSpace/';
-=======
-  // static APIpath = 'http://localhost:9001/OurSpace/';
->>>>>>> develop
-   static APIpath = 'http://ec2-54-145-139-170.compute-1.amazonaws.com:8080/';
-=======
+  static APIpath = 'http://ec2-54-145-139-170.compute-1.amazonaws.com:8080/';
 
-  static APIpath = 'http://localhost:9005/OurSpace/';
-  //  static APIpath = 'http://ec2-54-145-139-170.compute-1.amazonaws.com:8080/';
-
->>>>>>> develop
 
   constructor() { }
 }
